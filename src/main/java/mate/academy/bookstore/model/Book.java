a77f38fc10e34d0f4c1766bf4c4879aa2a56c4c2
Nazar <92024646+nklimovych@users.noreply.github.com>
2024-04-26 17:00:40 +0300

    Added implementation of Spring Data JPA (#5)
    * Replaced the existing BookRepository with JpaRepository interface. Add Liquibase support. Changed spring.jpa.hibernate.ddl-auto property value to validate. Implement soft delete concept for the Book entity. Add update and delete endpoints and methods for BookService.
    
    * Change a price type to decimal in database.
    
    * Added HttpStatus.NO_CONTENT as a return statement for deleteBook method. Change CreateBookRequestDto to BookRequestDto. Change deprecated annotation @Where to @SQLRestriction. Also add equals/hashcode and toString annotations for Book entity. Delete redundant liquibase change file.
    
    * Add columnDefinition = 'tinyint' constraint to isDeleted column
    
    * Code refactored, deleted isDeleted column constraint "columnDefinition = 'tinyint'". Change liquibase is_delete column type from boolean to bit.
    
    * Change mapping target from 'deleted' to 'isDeleted'
    
    * Code refactored in updateById method

--- src/main/java/mate/academy/bookstore/model/Book.java
+++ src/main/java/mate/academy/bookstore/model/Book.java
@@ -7,23 +7,40 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"id", "title", "author", "isbn"})
 @Entity
-@Data
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
+@SQLRestriction(value = "is_deleted=false")
 @Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private String author;
+
     @Column(nullable = false, unique = true)
     private String isbn;
+
     @Column(nullable = false)
     private BigDecimal price;
+
     private String description;
+
     private String coverImage;
+
+    @Column(nullable = false)
+    private Boolean isDeleted;
 }
